cd0236a7688cc7475f01dd37d4a37f470521fe0f
Avery <averywoodlief@gmail.com>
2026-05-26 14:03:05 -0500

    updated config

--- settings/config.py
+++ settings/config.py
@@ -2,11 +2,25 @@ import json
 
 settings = {}
 
-settings["screen"] = {"width" : 500, "height" : 500, "bg-color":(255, 255, 255)}
+settings["display"] = {}
+
+settings["display"]["screen-size"] = {"width" : 500, "height" : 500}
+
+settings["display"]["colors"] = {}
+settings["display"]["colors"]["color-white"] = (255, 255, 255)
+settings["display"]["colors"]["color-black"] = (0, 0, 0)
+settings["display"]["colors"]["color-grey"] = (50, 50, 50)
+settings["display"]["colors"]["color-bright-red"] = (255, 0, 0)
+settings["display"]["colors"]["color-bright-green"] = (0, 255, 0)
+settings["display"]["colors"]["color-bright-blue"] = (0, 0, 255)
+
+
+
+
 settings["general"] = {"frame cap":60}
-#settings["animation"] = {"frames":10, "lifetime":.5} # lifetime is in seconds # TODO
-#settings["animation"]["FPS"] = (settings["animation"]["frames"])/(settings["animation"]["lifetime"])
-settings["animation"] = {"FPS":60}
+settings["animation"] = {"FPS":60, "frames count":10}
+settings["animation"]["lifetime"] = settings["animation"]["frames count"]/settings["animation"]["FPS"] # lifetime measured in seconds
+settings["animation"]["smooth"] = False # if set to True, program will try to make the animation less 'blocky' in movement
 
 with open("config.json", "w+") as file:
     json.dump(settings, file, indent=4)
