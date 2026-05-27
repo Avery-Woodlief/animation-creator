13f511e7a9d9572c6b0b7fbd3e9e399a2ef90c15
Avery <averywoodlief@gmail.com>
2026-05-27 14:06:49 -0500

    added features

--- settings/config.py
+++ settings/config.py
@@ -1,10 +1,26 @@
 import json
+import pygame
+
+'''
+For the mouse
+
+    LEFT_MOUSE = 1
+    MIDDLE_MOUSE = 2
+    RIGHT_MOUSE = 3
+
+'''
+
+LEFT_MOUSE = 1
+MIDDLE_MOUSE = 2
+RIGHT_MOUSE = 3
+
+pygame.init()
 
 settings = {}
 
 settings["display"] = {}
 
-settings["display"]["screen-size"] = {"width" : 500, "height" : 500}
+settings["display"]["screen-size"] = {"width" : 1000, "height" : 700}
 
 settings["display"]["colors"] = {}
 settings["display"]["colors"]["color-white"] = (255, 255, 255)
@@ -14,12 +30,34 @@ settings["display"]["colors"]["color-bright-red"] = (255, 0, 0)
 settings["display"]["colors"]["color-bright-green"] = (0, 255, 0)
 settings["display"]["colors"]["color-bright-blue"] = (0, 0, 255)
 
-
-
-
 settings["general"] = {"frame cap":60}
-settings["animation"] = {"FPS":60, "frames count":10}
-settings["animation"]["lifetime"] = settings["animation"]["frames count"]/settings["animation"]["FPS"] # lifetime measured in seconds
+settings["general"]["key bindings"] = {}
+settings["general"]["key bindings"]["menu"] = {}
+
+settings["general"]["key bindings"]["menu"][pygame.K_1] = "play current work"
+settings["general"]["key bindings"]["menu"][pygame.K_2] = "drawing"
+settings["general"]["key bindings"]["menu"][pygame.K_3] = "selection"
+
+
+settings["general"]["key bindings"]["drawing-commands"] = {}
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LSHIFT and pygame.K_c] = "close shape"
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LCTRL and pygame.K_o] = "toggle overlay"
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LSHIFT and pygame.K_l and pygame.K_v] = "toggle vertical line"
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LSHIFT and pygame.K_l and pygame.K_h] = "toggle horizontal line"
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LCTRL and pygame.K_z] = "undo last node"
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LCTRL and pygame.K_LSHIFT and pygame.K_z] = "redo last undo"
+settings["general"]["key bindings"]["drawing-commands"][pygame.K_LCTRL and pygame.K_DELETE] = "delete selected node"
+
+settings["general"]["key bindings"]["animation-commands"] = {}
+settings["general"]["key bindings"]["animation-commands"][pygame.K_LCTRL and pygame.K_LSHIFT and pygame.K_r] = "reverse animation direction"
+settings["general"]["key bindings"]["animation-commands"][pygame.K_LCTRL and pygame.K_LSHIFT and pygame.K_c] = "customize animation direction" # TODO
+#settings["general"]["key bindings"]["animation-commands"][pygame.K_LCTRL and LEFT_MOUSE] = "select node" # TODO
+
+
+
+settings["animation"] = {"FPS-normal":60, "FPS-mouse-click":20, "frames count":10}
+settings["animation"]["FPS-mouse-drag"] = 60
+settings["animation"]["lifetime"] = settings["animation"]["frames count"]/settings["animation"]["FPS-normal"] # lifetime measured in seconds
 settings["animation"]["smooth"] = False # if set to True, program will try to make the animation less 'blocky' in movement
 settings["animation"]["points per segment"] = 5 # this is effectively how detailed each line will be for smoothness
 
