e59a1bfe3cfafcb41165302832c6401607bbd516
Avery <averywoodlief@gmail.com>
2026-05-29 21:40:48 -0500

    cursor is now dynamic

--- code/window.py
+++ code/window.py
@@ -15,12 +15,20 @@ class Window:
         for button in self.children["buttons"]:
             if (not button):
                 continue
+            if (not button.clickable):
+                continue
             if (button.check_state(event) == "clicked" and (event.type != pygame.MOUSEBUTTONUP)):
+                self.settings["mouse-settings"]["active cursor"] = pygame.SYSTEM_CURSOR_HAND
+                #print("click button down")
+                #self.settings["mouse-settings"]["active cursor"] = pygame.SYSTEM_CURSOR_HAND
                 try:
                     events.remove(event)
                 except (ValueError): # already removed
                     return button.action()
                 return button.action()
+            elif (button.check_state(event) == "hovering"):
+                self.settings["mouse-settings"]["active cursor"] = pygame.SYSTEM_CURSOR_HAND
+                #print("hovering")
         return
 
     def kill(self): # for semantics
